8716882e001f9a343606594c467d34985ee7df26
montoshitaa <kristel.montoya.chaves@est.una.ac.cr>
2026-06-15 12:02:38 -0600

    feat: add Playwright and related dependencies to development environment

--- flake.nix
+++ flake.nix
@@ -20,13 +20,48 @@
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
+            # Node
             nodejs
+            pnpm
+
+            # TypeScript
             typescript
             typescript-language-server
+
+            # Calidad de código
             eslint
             prettier
             cspell
-            pnpm
+
+            # Dependencias para Playwright / Chromium
+            glib
+            gtk3
+            cairo
+            pango
+            atk
+
+            dbus
+            alsa-lib
+
+            nss
+            nspr
+
+            fontconfig
+            freetype
+
+            libdrm
+            mesa
+
+            xorg.libX11
+            xorg.libXcomposite
+            xorg.libXcursor
+            xorg.libXdamage
+            xorg.libXext
+            xorg.libXfixes
+            xorg.libXi
+            xorg.libXrandr
+            xorg.libXrender
+            xorg.libxcb
           ];
 
           shellHook = ''
